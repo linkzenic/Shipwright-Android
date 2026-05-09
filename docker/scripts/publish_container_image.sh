c354680eb044c9ea11f4a05d2bc5182d162ac09e
John Schmitz <schmitty247@gmail.com>
2026-05-09 17:47:27 -0500

    add Android release CI workflow with build attestation and Docker image v1.0.1

--- docker/scripts/publish_container_image.sh
+++ docker/scripts/publish_container_image.sh
@@ -3,20 +3,12 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source ${DIR}/set_container_vars.sh
 
-IMAGE_NAME="${IMAGE_NAME}:${IMAGE_TAG}"
 REGISTRY_IMAGE="${REGISTRY_REPO}:${IMAGE_TAG}"
 
 echo ""
 echo "-----------------------------------------------------------"
 echo "-- Publishing Container Image                            --"
 echo "-----------------------------------------------------------"
-echo "Tagging '${IMAGE_NAME}' as '${REGISTRY_IMAGE}'..."
-docker tag ${IMAGE_NAME} ${REGISTRY_IMAGE}
-if [[ $? -ne 0 ]]; then
-    echo "ERROR: Tag failed."
-    exit 1
-fi
-
 echo "Pushing '${REGISTRY_IMAGE}'..."
 docker push ${REGISTRY_IMAGE}
 if [[ $? -ne 0 ]]; then
